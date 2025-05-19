02f8e92949bc04b697fbf2cc3fa9a11b006bd75d
benja2998 <benja2998@noreply.codeberg.org>
2025-05-18 22:28:18 -0400

    fix syntax

--- packages/bash-demo/bash-demo.sh
+++ packages/bash-demo/bash-demo.sh
@@ -5,23 +5,20 @@ world="World!"
 
 echo $hello $world
 
-echo "This is a demonstration of basic bash syntax. The source code will be shown shortly..."
-sleep 3
-cat ./bash-demo.sh
+echo "This is a demonstration of basic bash scripting."
 sleep 1
-echo "Now, let's do math"
-sleep 1
+echo "Math:"
 echo "1 + 1 = $((1 + 1))"
 sleep 1
 echo "2 * 2 = $((2 * 2))"
 sleep 1
 echo "10 / 2 = $((10 / 2))"
 sleep 1
-echo "Let's do a loop:"
+echo "Loop:"
 sleep 1
 count = 0
-while [ $count -lt 10 ] {
+while [ $count -lt 10 ] (
     $count = $count + 1
     echo $count
     sleep 0.5
-}
+)
