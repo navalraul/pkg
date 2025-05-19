d20f35505079b28b6032fd09a841abf3f07b3b56
benja2998 <benja2998@noreply.codeberg.org>
2025-05-18 22:29:44 -0400

    fix syntax

--- packages/bash-demo/bash-demo.sh
+++ packages/bash-demo/bash-demo.sh
@@ -16,9 +16,10 @@ echo "10 / 2 = $((10 / 2))"
 sleep 1
 echo "Loop:"
 sleep 1
-count = 0
-while [ $count -lt 10 ] (
-    $count = $count + 1
+count=0
+while [ $count -lt 10 ]
+do
+    count=$((count + 1))
     echo $count
     sleep 0.5
-)
+done
